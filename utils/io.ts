139f744110d31b064e3590251fbcbdace307a9eb
Stefano Merazzi <asso99@hotmail.com>
2022-11-30 21:54:29 +0100

    feat: add readline helper methods

--- utils/io.ts
+++ utils/io.ts
@@ -1,4 +1,26 @@
+import { readline } from "https://deno.land/x/readline@v1.1.0/mod.ts";
+
 export async function readTextFile(input: string): Promise<string> {
   const text = await Deno.readTextFile(input);
   return text;
+}
+
+export async function readLinesIntoStringArray(input: string): Promise<string[]> {
+  const array = [];
+  const f = await Deno.open(input);
+  for await (const line of readline(f)) {
+    array.push(new TextDecoder().decode(line));
+  }
+  f.close();
+  return array;
+}
+
+export async function readLinesIntoNumberArray(input: string): Promise<number[]> {
+  const array = [];
+  const f = await Deno.open(input);
+  for await (const line of readline(f)) {
+    array.push(parseInt(new TextDecoder().decode(line), 10));
+  }
+  f.close();
+  return array;
 }
